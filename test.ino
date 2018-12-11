ac6baa7320619a277335db011496513b1a915daf
cjm0904 <hfo@nate.com>
2018-12-11 16:43:10 +0900

    Create test.ino

---
+++ test.ino
@@ -0,0 +1,30 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial mySerial(3,2);
+
+void setup()
+{
+  Serial.begin(9600);
+  while (!Serial){
+  ;
+  }
+  
+  Serial.println("Goodnight moon!");
+  mySerial.begin(9600);
+}
+
+void loop(){
+  int arr[17];
+  int i=0;
+  
+  if(mySerial.available()){
+    for(i=0; i<17;i++){
+      arr[i] = mySerial.read();
+      Serial.print(arr[i]);
+    }
+  }
+  
+  int arr[17] = {0,};
+  Serial.println("");
+  delay(2000);
+}
