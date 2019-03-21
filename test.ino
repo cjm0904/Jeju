b0bd359cd3e751540b7ee04780ddd340ab3ce259
cjm0904 <hfo@nate.com>
2019-03-21 16:49:38 +0900

    Update test.ino

--- test.ino
+++ test.ino
@@ -1,5 +1,5 @@
 #include <SoftwareSerial.h>
-#define CNUM 1
+#define CNUM "C001"
 
 SoftwareSerial mySerial(2,3);
 
@@ -16,31 +16,24 @@ void setup()
 void loop(){
   int arr[17];
   int i=0;
-  
+  String data = "";
+  data += CNUM;
   if(mySerial.available()){
     for(i=0; i<sizeof(arr)/sizeof(int);i++){
       arr[i] = mySerial.read();
-      Serial.print(arr[i]);
-//      if(arr[sizof(arr)/sizeof(int) - 1] != 10){
-//        i=0;
-//      }
-//      Serial.println(i);
-//      Serial.println(arr[i]);
-//      Serial.println("----------------");
+      data += arr[i];
+//      Serial.print(arr[i]);
       if(i == 16){
         if(arr[16] != 10){
           i=0;
-          Serial.print("HI");
-          Serial.println(""); 
-          arr[17] = {0,};
+          arr[17] = {0,}; 
         }
         else{
-          Serial.print("HELLO");
+          Serial.print(data);
         }
       }
     }
   }
-  Serial.println("");
   arr[17] = {0,};
   delay(3000);
 }
